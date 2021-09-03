055235da0fb9a71b7e0767d71c594b6ae165effd
Nikhil Nagrale <nknagrale634@gmail.com>
2021-09-03 21:45:09 +0530

    firebase config

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCpDp3-nHyxlRetEdlShN7Atzmn3eaMVI",
+  authDomain: "amazecart-e1ef0.firebaseapp.com",
+  projectId: "amazecart-e1ef0",
+  storageBucket: "amazecart-e1ef0.appspot.com",
+  messagingSenderId: "583312589441",
+  appId: "1:583312589441:web:4a3dcd1595af5ffda70380",
+  measurementId: "G-XDMQE4PY4X",
+};
