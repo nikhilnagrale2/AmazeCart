f5b0d9e11434d656c7e3cc9f3e860d779df46261
Nikhil Nagrale <nknagrale634@gmail.com>
2021-09-05 12:55:00 +0530

    webhook for firestore and checkout session added

---
+++ src/pages/api/create-checkout-session.js
@@ -0,0 +1,36 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async (req, res) => {
+  const { items, email } = req.body;
+  //   console.log(items, email);
+  const transformedItems = items.map((item) => ({
+    description: items.description,
+    quantity: 1,
+    price_data: {
+      currency: "inr",
+      unit_amount: item.price * 100 * 50,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+      },
+    },
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_rates: ["shr_1JVvDySB3vSiV3VJXrTz6KtQ"],
+    shipping_address_collection: {
+      allowed_countries: ["GB", "US", "CA", "IN"],
+    },
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
+};
